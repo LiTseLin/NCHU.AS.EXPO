54b4719b70f90bb0580a0812eadb446597802ef3
LiTseLin <tachinori@smail.nchu.edu.tw>
2020-04-06 02:05:35 +0800

    anothercss

---
+++ wp-content/plugins/elementor/assets/js/elementorFrontendConfig.js
@@ -0,0 +1,33 @@
+var elementorFrontendConfig = {
+    "environmentMode": {
+        "edit": false,
+        "wpPreview": false
+    },
+    "i18n": {
+        "shareOnFacebook": "Share on Facebook",
+        "shareOnTwitter": "Share on Twitter",
+        "pinIt": "Pin it",
+        "downloadImage": "Download image"
+    },
+    "breakpoints": {
+        "xs": 0,
+        "sm": 480,
+        "md": 768,
+        "lg": 1025,
+        "xl": 1440,
+        "xxl": 1600
+    },
+    "settings": {
+        "page": [],
+        "general": {
+            "elementor_global_image_lightbox": "yes",
+            "elementor_lightbox_enable_counter": "yes",
+            "elementor_lightbox_enable_fullscreen": "yes",
+            "elementor_lightbox_enable_zoom": "yes",
+            "elementor_lightbox_enable_share": "yes",
+            "elementor_lightbox_title_src": "title",
+            "elementor_lightbox_description_src": "description"
+        },
+        "editorPreferences": []
+    },
+};
